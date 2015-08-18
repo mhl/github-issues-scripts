14d7989145dbc58d28967624de5c12963c211626
Mark Longair <mhl@pobox.com>
2015-08-18 08:24:07 +0100

    Add a script for setting the estimated difficulties

---
+++ set-estimates.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import csv
+import doctest
+import json
+from optparse import OptionParser
+import os
+from os.path import dirname, join, realpath
+import requests
+
+with open(join(os.environ['HOME'], '.github-oauth-token-new.json')) as f:
+    token = json.load(f)['token']
+
+standard_headers = {'User-Agent': 'github-issues-printer/1.0',
+                    'Authorization': 'bearer {0}'.format(token)}
+
+cwd = os.getcwd()
+repo_directory = realpath(join(dirname(__file__)))
+
+def main(repo, csv_file):
+
+    with open(csv_file) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            issue_number = row['Number']
+            try:
+                difficulty = int(row['Consensus'], 10)
+            except ValueError:
+                continue
+
+            print "{0} => {1}".format(issue_number, difficulty)
+
+            url_template = 'https://api.github.com/repos/{0}/issues/{1}/labels'
+            url = url_template.format(repo, issue_number)
+            labels = ["Difficulty " + str(difficulty)]
+            print "url:", url
+            print "labels:", labels
+            r = requests.post(url,
+                              headers=standard_headers,
+                              data=json.dumps(labels))
+            print r.content
+
+usage = """Usage: %prog [options] REPOSITORY CSV-FILE
+
+REPOSITORY should be username/repository from GitHub, e.g. mysociety/pombola
+
+CSV-FILE should be have a "Consensus" column (with the estimated
+complexity as an integer) and a "Number" column with the issue number.
+"""
+parser = OptionParser(usage=usage)
+parser.add_option("-t", "--test",
+                  action="store_true", dest="test", default=False,
+                  help="Run doctests")
+
+(options, args) = parser.parse_args()
+
+if options.test:
+    doctest.testmod()
+else:
+    if len(args) != 2:
+        parser.print_help()
+    else:
+        main(args[0], args[1])
